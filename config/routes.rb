8df8bf180282937215f1bbdd8214ac3517e0f75a
tak400 <ogttkms@gmail.com>
2019-08-29 22:52:34 +0900

    edit group route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:edit, :new, :create, :update] do
+  resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
 end
