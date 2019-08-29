09854c1413fd040dcce65ee0d266db34c489740e
tak400 <ogttkms@gmail.com>
2019-08-29 22:28:48 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'messages#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:edit, :new, :create, :update] do
+    resources :messages, only: [:index, :create]
 end
