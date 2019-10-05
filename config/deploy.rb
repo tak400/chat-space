d484fae7f5adf2bcaebe6d9b50e71db394e3d520
tak400 <ogttkms@gmail.com>
2019-10-05 16:04:34 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.2"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:tak400/chat-space.git"
+set :repo_url, "git@github.com:tak400/chat-space.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
