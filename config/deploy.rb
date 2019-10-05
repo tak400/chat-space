4c56c4aacbac0872edce8dd699aaf2bf07ce7814
tak400 <ogttkms@gmail.com>
2019-10-05 17:27:29 +0900

    edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/.ssh/gravity.pem']
+                  keys: ['~/.ssh/gravity.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
