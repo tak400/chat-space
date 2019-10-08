4611d1004f64bf12135b5aad79c7f0ee0a991d2a
tak400 <ogttkms@gmail.com>
2019-10-08 15:19:21 +0900

    edit carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'chta-space.tak400'
+  config.fog_directory  = 'chat-space.tak400'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space.tak400'
 end
